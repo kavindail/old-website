05b652ddd16abacd8713263dd863bf622d049f75
Kavin <kavindail17@gmail.com>
2023-03-18 09:46:34 -0700

    added smooth scrolling and navbar functionality

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ window.addEventListener("scroll", () => {
 	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+/*
+	Smooth scroll functionality for anchor links (animates the scroll
+	rather than a sudden jump in the page)
+*/
